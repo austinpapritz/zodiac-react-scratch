5f9c0dfac9e3215c83c112d847c333677d87e05f
Austin Papritz <austin@papritz.dev>
2023-01-09 17:21:19 -0800

    background showing, rcf for footer and header

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './Main.css';
 import background from '../../background.png';
-import { zodiac } from '../../data.js';
-import Zodiac from '../Zodiac/Zodiac.js';
+
+// import { zodiac } from '../../data.js';
+// import Zodiac from '../Zodiac/Zodiac.js';
 
 export default function Main() {
-  return <main style={{ backgroundImage: `url(${background})` }}></main>;
+  return <main style={{ backgroundImage: `url('${background}')` }}></main>;
 }
