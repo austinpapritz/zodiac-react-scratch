14b25449d2f9dce65cd2b7fd040523afd936ae16
Austin Papritz <austin@papritz.dev>
2023-01-09 16:34:30 -0800

    data.js filled with zodiac info

---
+++ src/data.js
@@ -0,0 +1,62 @@
+export const animals = [
+  {
+    name: 'Aquarius',
+    icon_url: './public/zodiac_images/aquarius.png',
+    date: 'Jan 21 - Feb 19',
+  },
+  {
+    name: 'Aries',
+    icon_url: './public/zodiac_images/aries.png',
+    date: 'Mar 21 - Apr 19',
+  },
+  {
+    name: 'Cancer',
+    icon_url: './public/zodiac_images/cancer.png',
+    date: 'Jun 21 - Jul 22',
+  },
+  {
+    name: 'Capricorn',
+    icon_url: './public/zodiac_images/capricorn.png',
+    date: 'Dec 22 - Feb 19',
+  },
+  {
+    name: 'Gemini',
+    icon_url: './public/zodiac_images/gemini.png',
+    date: 'May 21 - Jun 20',
+  },
+  {
+    name: 'Leo',
+    icon_url: './public/zodiac_images/leo.png',
+    date: 'Jul 23 - Aug 22',
+  },
+  {
+    name: 'Libra',
+    icon_url: './public/zodiac_images/libra.png',
+    date: 'Sept 23 - Oct 22',
+  },
+  {
+    name: 'Pisces',
+    icon_url: './public/zodiac_images/pisces.png',
+    date: 'Feb 19 - Mar 20',
+  },
+  {
+    name: 'Sagittarius',
+    icon_url: './public/zodiac_images/sagittarius.png',
+    date: 'Nov 22 - Dec 21',
+  },
+  {
+    name: 'Scorpio',
+    icon_url: './public/zodiac_images/scorpio.png',
+    date: 'Oct 23 - Nov 21',
+  },
+  {
+    name: 'Taurus',
+    icon_url: './public/zodiac_images/taurus.png',
+    date: 'Apr 20 - May 20',
+  },
+  {
+    name: 'Virgo',
+    icon_url: './public/zodiac_images/virgo.png',
+    date: 'Aug 23 - Sep 22',
+  },
+];
